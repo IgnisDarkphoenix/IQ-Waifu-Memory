7748a75cbda85afce74dc5db3488d4441dc21426
IgnisDarkphoenix <167255282+IgnisDarkphoenix@users.noreply.github.com>
2025-12-30 05:15:59 -0800

    Fix: Memory leak in InputProcessor

--- core/src/com/waifu/memory/screens/BaseScreen.java
+++ core/src/com/waifu/memory/screens/BaseScreen.java
@@ -1,6 +1,8 @@
+
 package com.waifu.memory.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -9,53 +11,82 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.waifu.memory.IQWaifuMemory;
 import com.waifu.memory.data.PlayerData;
-import com.waifu.memory.managers.AssetManager;
 import com.waifu.memory.managers.AudioManager;
 import com.waifu.memory.utils.Constants;
 
+/**
+ * Clase base para todas las pantallas del juego
+ * FIXED: Memory leak de InputProcessor
+ */
 public abstract class BaseScreen implements Screen {
-
+    
     protected final IQWaifuMemory game;
     protected final SpriteBatch batch;
     protected final OrthographicCamera camera;
     protected final Viewport viewport;
-    protected final AssetManager assetManager;
+    
+    protected final com.waifu.memory.managers.AssetManager assetManager;
     protected final AudioManager audioManager;
-
+    
+    // FIX: Mantener referencia al InputProcessor para limpiarlo
+    protected InputProcessor inputProcessor;
+    
+    private static final float TRANSITION_DURATION = Constants.TRANSITION_DURATION;
+    private float transitionAlpha = 0f;
+    private boolean transitionIn = true;
+    private BaseScreen nextScreen = null;
+    
     public BaseScreen(IQWaifuMemory game) {
         this.game = game;
         this.batch = game.getBatch();
         this.assetManager = game.getAssetManager();
         this.audioManager = game.getAudioManager();
-
-        camera = new OrthographicCamera();
-        viewport = new FitViewport(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT, camera);
-        camera.position.set(Constants.WORLD_WIDTH / 2f, Constants.WORLD_HEIGHT / 2f, 0);
-        camera.update();
+        
+        this.camera = new OrthographicCamera();
+        this.viewport = new FitViewport(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT, camera);
+        this.camera.position.set(Constants.WORLD_WIDTH / 2, Constants.WORLD_HEIGHT / 2, 0);
+        this.camera.update();
+        
+        this.inputProcessor = null;
     }
-
+    
+    /**
+     * FIX: Método helper para establecer el InputProcessor de forma segura
+     * Las subclases deben llamar a este método en lugar de Gdx.input.setInputProcessor
+     */
+    protected void setInputProcessor(InputProcessor processor) {
+        this.inputProcessor = processor;
+        Gdx.input.setInputProcessor(processor);
+    }
+    
+    /**
+     * FIX: Limpia el InputProcessor actual
+     */
+    private void clearInputProcessor() {
+        if (inputProcessor != null) {
+            if (Gdx.input.getInputProcessor() == inputProcessor) {
+                Gdx.input.setInputProcessor(null);
+            }
+            inputProcessor = null;
+        }
+    }
+    
     protected PlayerData getPlayerData() {
         return game.getPlayerData();
     }
-
+    
     protected void saveProgress() {
         game.savePlayerData();
     }
-
-    protected void goToScreen(BaseScreen newScreen) {
-        // Limpiar input processor antes de cambiar
-        Gdx.input.setInputProcessor(null);
-        game.setScreen(newScreen);
+    
+    protected void goToScreen(BaseScreen screen) {
+        nextScreen = screen;
+        transitionIn = false;
+        transitionAlpha = 0f;
     }
-
-    @Override
-    public void show() {
-        // Subclases pueden override
-    }
-
+    
     @Override
     public void render(float delta) {
-        // Limpiar pantalla
         Gdx.gl.glClearColor(
             Constants.COLOR_BACKGROUND[0],
             Constants.COLOR_BACKGROUND[1],
@@ -63,39 +94,65 @@ public abstract class BaseScreen implements Screen {
             Constants.COLOR_BACKGROUND[3]
         );
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        // Habilitar blending globalmente
-        Gdx.gl.glEnable(GL20.GL_BLEND);
-        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-
+        
+        if (transitionIn || nextScreen == null) {
+            update(delta);
+        }
+        
         camera.update();
         batch.setProjectionMatrix(camera.combined);
-
-        update(delta);
+        
         draw();
+        
+        updateTransition(delta);
     }
-
+    
+    private void updateTransition(float delta) {
+        if (transitionIn) {
+            transitionAlpha += delta / TRANSITION_DURATION;
+            if (transitionAlpha >= 1f) {
+                transitionAlpha = 1f;
+                transitionIn = false;
+            }
+        } else if (nextScreen != null) {
+            transitionAlpha += delta / TRANSITION_DURATION;
+            if (transitionAlpha >= 1f) {
+                game.setScreen(nextScreen);
+                dispose();
+            }
+        }
+    }
+    
     protected abstract void update(float delta);
     protected abstract void draw();
-
+    
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height, true);
-        camera.position.set(Constants.WORLD_WIDTH / 2f, Constants.WORLD_HEIGHT / 2f, 0);
+        camera.position.set(Constants.WORLD_WIDTH / 2, Constants.WORLD_HEIGHT / 2, 0);
     }
-
+    
+    @Override
+    public void show() {
+        transitionIn = true;
+        transitionAlpha = 0f;
+    }
+    
     @Override
     public void pause() {}
-
+    
     @Override
     public void resume() {}
-
+    
     @Override
     public void hide() {
-        // Limpiar input al ocultar pantalla
-        Gdx.input.setInputProcessor(null);
+        // FIX: Limpiar el InputProcessor cuando la pantalla se oculta
+        clearInputProcessor();
     }
-
+    
     @Override
-    public void dispose() {}
+    public void dispose() {
+        // FIX: Asegurarse de limpiar el InputProcessor al dispose
+        clearInputProcessor();
+    }
 }
